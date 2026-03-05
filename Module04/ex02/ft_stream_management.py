aa7d63e28e7d77aa84f9fe0559bfbd7f93356db4
Soufian Boudchar <sboudcha@c3r1p5.1337.ma>
2026-03-05 20:21:18 +0000

    update

--- Module04/ex02/ft_stream_management.py
+++ Module04/ex02/ft_stream_management.py
@@ -3,7 +3,12 @@ print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
 
 
 
-arch_input("Input Stream active. Enter archivist ID: ")
+a = input("Input Stream active. Enter archivist ID: ")
+s = input("Input Stream active. Enter status report: ")
 
 
-status = input("Input Stream active. Enter status report: ")
+sys.stdout.write("\n{[}STANDARD{]} Archive status from ARCH_7742: " + a + "\n")
+
+sys.stderr.write("{[}ALERT{]} System diagnostic: " + s + "\n")
+sys.stdout.write("{[}STANDARD{]} Data transmission complete")
+print("\n\nThree-channel communication test successful.")
