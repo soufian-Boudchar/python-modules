54eb878892903e19118e727c38a7d4cd68c4fa82
Soufian Boudchar <sboudcha@c3r1p5.1337.ma>
2026-03-05 19:53:58 +0000

    update

---
+++ Module04/ex02/ft_stream_management.py
@@ -0,0 +1,9 @@
+import sys
+print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
+
+
+
+arch_input("Input Stream active. Enter archivist ID: ")
+
+
+status = input("Input Stream active. Enter status report: ")
